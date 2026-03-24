9d3f87410a74b45b2e025a736c09a981c422a18e
MathlaAlkuwari <mathlaalkuwari.23@gmail.com>
2026-03-24 15:10:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @returns {number} The sum of a and b.
  */
 function sum(a, b) {
-  // TODO: Implement this function.
+  return a+b;
 }
 
 /**
@@ -12,7 +12,12 @@ function sum(a, b) {
  * @returns {string} The reversed string.
  */
 function reverseString(str) {
-  // TODO: Implement this function.
+let reversed ="";
+for (let i = str.length -1; i>=0 ; i--)
+{
+  reversed += str[i];
+}
+return reversed;
 }
 
 /**
@@ -20,7 +25,18 @@ function reverseString(str) {
  * @returns {number} The largest number in the array. Return null if the array is empty.
  */
 function findLargest(numbers) {
-  // TODO: Implement this function.
+ if (numbers.length === 0) {
+  return null;
+ }
+ let largest =numbers[0];
+ for (let i =1; i <numbers.length;i++)
+ {
+  if(numbers[i] > largest)
+  {
+    largest = numbers[i];
+  }
+ }
+ return largest;
 }
 
 /**
@@ -30,15 +46,31 @@ function findLargest(numbers) {
  * The check should be case-insensitive.
  */
 function isPalindrome(str) {
-  // TODO: Implement this function.
-}
+  let lowerStr = str.toLowerCase();
+  let reversed ="";
+  
+  for (let i =lowerStr.length -1; i>=0; i--)
+  {
+    reversed+=lowerStr[i];
+  }
+  return lowerStr === reversed;
+ }
 
 /**
  * @param {number[]} numbers An array of numbers.
  * @returns {number[]} A new array containing only the even numbers from the original array.
  */
 function filterEvenNumbers(numbers) {
-  // TODO: Implement this function.
+  let evenNumbers =[];
+
+  for (let i=0; i<numbers.length; i++)
+  {
+    if (numbers[i]%2 ===0)
+    {
+      evenNumbers.push(numbers[i])
+    }
+  }
+  return evenNumbers;
 }
 
 
